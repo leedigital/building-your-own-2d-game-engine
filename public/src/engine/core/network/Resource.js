bd7da881305ac1b8fe155ef6f58dc6f7cd288475
Vagner Araujo <vagner@leedigital.com.br>
2020-08-04 15:39:43 -0300

    building-2d-game-engine

--- public/src/engine/core/network/Resource.js
+++ public/src/engine/core/network/Resource.js
@@ -36,22 +36,55 @@ class Resource {
      * 
      * @param {Function} callback 
      */
-    load(callback) {
+    async load(callback) {
         const resources = this.resources;
         if (resources.length === 0) {
             callback(this);
         } else {
             const resource = resources.pop();
             const name = resource.name;
+            /**
+             * @type {String}
+             */
             const url = resource.url;
             if (/(\.gif|\.png|\.jpg|\.jpeg)$/gi.test(url)) {
+                const res = await fetch(url);
+                const blob = await res.blob();
+                const uri = URL.createObjectURL(blob);
                 const image = new Image;
                 image.onload = () => {
                     this.cache.set(name, image);
                     this.load(callback);
                 };
-                image.src = url;
+                image.src = uri;
             } else if (/(\.json)$/gi.test(url)) {
+                const res = await fetch(url);
+                const json = await res.json();
+                const frames = json.frames;
+                const meta = json.meta;
+                if (frames && meta) {
+                    const imageUrl = `${url.substring(0, url.lastIndexOf('/'))}/${meta.image}`;
+                    const res = await fetch(imageUrl);
+                    const blob = await res.blob();
+                    const uri = URL.createObjectURL(blob);
+                    const image = new Image;
+                    image.onload = () => {
+                        for (const i in frames) {
+                            const frameObject = frames[i];
+                            const frame = frameObject.frame;
+                            this.cache.set(i, {
+                                x: frame.x,
+                                y: frame.y,
+                                w: frame.w,
+                                h: frame.h,
+                                image: image
+                            });
+
+                        }
+                        this.load(callback);
+                    };
+                    image.src = uri;
+                }
 
             } else if (/(\.mp3|\.ogg|\.wav)$/gi.test(url)) {
 
